c84f8a3c85482908a9c9b405bc84ea496a2c2e83
xiaosayin <811469225@qq.com>
2023-10-11 22:05:58 +0800

    first commit

---
+++ mpd/CMakeFiles/MPDtest.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/MPDtest.cpp" "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/CMakeFiles/MPDtest.dir/MPDtest.cpp.o"
+  "/home/yinwenpei/mmsys23lib-dev-xzc/demo/demotransportcontroller.cpp" "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/CMakeFiles/MPDtest.dir/__/demo/demotransportcontroller.cpp.o"
+  "/home/yinwenpei/mmsys23lib-dev-xzc/demo/utils/defaultclock.cpp" "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/CMakeFiles/MPDtest.dir/__/demo/utils/defaultclock.cpp.o"
+  "/home/yinwenpei/mmsys23lib-dev-xzc/demo/utils/thirdparty/quiche/quic_clock.cpp" "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/CMakeFiles/MPDtest.dir/__/demo/utils/thirdparty/quiche/quic_clock.cpp.o"
+  "/home/yinwenpei/mmsys23lib-dev-xzc/demo/utils/thirdparty/quiche/quic_time.cpp" "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/CMakeFiles/MPDtest.dir/__/demo/utils/thirdparty/quiche/quic_time.cpp.o"
+  "/home/yinwenpei/mmsys23lib-dev-xzc/demo/utils/thirdparty/quiche/rtt_stats.cpp" "/home/yinwenpei/mmsys23lib-dev-xzc/mpd/CMakeFiles/MPDtest.dir/__/demo/utils/thirdparty/quiche/rtt_stats.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CACHE_SIZE_16K=50000"
+  "LINUX_X86_PLAT"
+  "OPEN_LAB_TRACE"
+  "PARTNER_ISURECLOUD"
+  "SPDLOG_LEVEL_TRACE=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "mpd"
+  "mpd/app"
+  "mpd/basefw"
+  "mpd/basefw/base"
+  "mpd/download/transportcontroller"
+  "mpd/third_lib/json/single_include"
+  "demo/utils"
+  "demo"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
